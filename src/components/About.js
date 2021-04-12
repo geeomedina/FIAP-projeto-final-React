0c4be460777d48ab1b4095c590f08a839ba669b4
Francisco <francisco.olvera@finanzero.com.br>
2021-04-12 18:22:11 -0300

    Tirando css e botando styled components

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,19 @@
-import './About.css';
-import { Title } from "./styled";
+import { Title, AboutDiv, AboutDescription, AboutFooter } from "./styled";
 
 function About() {
     return (
-      <div className="About">
+      <AboutDiv>
           <Title>To Do List</Title>
 
-          <div className="about__description">Gerencie suas tarefas ou anotações neste app.</div>
+          <AboutDescription>Gerencie suas tarefas ou anotações neste app.</AboutDescription>
 
           <div>Felipe Oliveira - RM 338405</div>
           <div>Francisco Olvera - RM 338050</div>
           <div>Geovane Medina - RM 338045</div>
           <div>Marcello Chuahy - RM 337780</div>
 
-          <div className="about__footer">Todos os direitos reservados. © 2021</div>
-      </div>
+          <AboutFooter>Todos os direitos reservados. © 2021</AboutFooter>
+      </AboutDiv>
     );
   }
   
